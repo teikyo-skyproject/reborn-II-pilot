bc3378d2b3763f87873b2cf6cd391de6aa07f950
Watson-Sei <55475145+Watson-Sei@users.noreply.github.com>
2023-05-20 16:13:46 +0900

    add flutter_map and details plane data show

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,29 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_map/plugin_api.dart';
 import 'package:flutter_map/flutter_map.dart';
 import 'package:latlong2/latlong.dart';
+import 'package:geolocator/geolocator.dart';
 
-void main() => runApp(MyApp());
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setPreferredOrientations([
+    DeviceOrientation.landscapeLeft,
+    DeviceOrientation.landscapeRight,
+  ]).then((_) {
+    runApp(Main());
+  });
+}
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+class Main extends StatefulWidget {
+  @override
+  _MainState createState() => _MainState();
+}
+
+class _MainState extends State<Main> {
+  LatLng _center = LatLng(35.681236, 139.767125);
+  String speed = '速度: 0000.0000';
+  String power = 'パワー: 0000.0000';
 
   @override
   Widget build(BuildContext context) {
@@ -14,24 +31,82 @@ class MyApp extends StatelessWidget {
       title: 'reborn-II-pilot',
       theme: ThemeData.light(useMaterial3: true),
       home: Scaffold(
-          appBar: AppBar(
-            title: Text('reborn-II-pilot'),
+        body: FlutterMap(
+          options: MapOptions(
+            center: _center,
+            zoom: 10.0,
+            interactiveFlags: InteractiveFlag.all,
+            enableScrollWheel: true,
+            scrollWheelVelocity: 0.00001,
           ),
-          body: FlutterMap(
-            options: MapOptions(
-              center: LatLng(35.2401, 136.0210),
-              zoom: 10.0,
-              interactiveFlags: InteractiveFlag.all,
-              enableScrollWheel: true,
-              scrollWheelVelocity: 0.00001,
+          children: [
+            TileLayer(
+              urlTemplate: "https://tile.openstreetmap.jp/{z}/{x}/{y}.png",
+              userAgentPackageName: 'land_place',
             ),
-            children: [
-              TileLayer(
-                urlTemplate: "https://tile.openstreetmap.jp/{z}/{x}/{y}.png",
-                userAgentPackageName: 'land_place',
-              )
-            ],
-          )),
+            Align(
+              alignment: Alignment.bottomRight,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  Padding(
+                    padding: EdgeInsets.all(15.0),
+                    child: ElevatedButton(
+                        onPressed: () {},
+                        style: ElevatedButton.styleFrom(
+                          primary: Colors.blue[300],
+                          shape: CircleBorder(),
+                          padding: EdgeInsets.all(20),
+                        ),
+                        child: Icon(
+                          Icons.location_on,
+                          color: Colors.white,
+                          size: 30,
+                        )),
+                  )
+                ],
+              ),
+            ),
+            Align(
+              alignment: Alignment.bottomLeft,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  Padding(
+                    padding: EdgeInsets.only(bottom: 30, left: 20),
+                    child: Container(
+                      color: Colors.white,
+                      child: Column(
+                        children: [
+                          Padding(
+                            padding: EdgeInsets.all(10),
+                            child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Text(
+                                    speed,
+                                    style: TextStyle(
+                                      fontSize: 20,
+                                    ),
+                                  ),
+                                  Text(
+                                    power,
+                                    style: TextStyle(
+                                      fontSize: 20,
+                                    ),
+                                  ),
+                                ]),
+                          )
+                        ],
+                      ),
+                    ),
+                  )
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
     );
   }
 }
